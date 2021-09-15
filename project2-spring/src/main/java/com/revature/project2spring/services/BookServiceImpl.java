7780c1448610f69969af1b63cef9ebb476e412ba
c-ramirez6 <chris.ramirez@revature.net>
2021-09-15 11:14:58 -0400

    Added Book repository, service, and controller functionality

--- project2-spring/src/main/java/com/revature/project2spring/services/BookServiceImpl.java
+++ project2-spring/src/main/java/com/revature/project2spring/services/BookServiceImpl.java
@@ -1,5 +1,27 @@
 package com.revature.project2spring.services;
 
-public class BookServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.project2spring.entities.Book;
+import com.revature.project2spring.repositories.BookRepository;
+
+@Service
+public class BookServiceImpl implements BookService{
+
+	@Autowired
+	private BookRepository repository;
+	
+	@Override
+	public List<Book> getAllBook() {
+		return repository.findAll();
+	}
+
+	@Override
+	public List<Book> getAllBookByBookTitleContaining(String title) {
+		return repository.findAllBookByTitleContaining(title);
+	}
 
 }
