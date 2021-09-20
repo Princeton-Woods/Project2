564a1384f719e5d8104edaf25620f80ef522179f
CCunningham0 <caleb.cunningham@revature.net>
2021-09-19 20:59:45 -0400

    Update book-detail buton

--- project2-angular/src/app/components/book-detail/book-detail.component.ts
+++ project2-angular/src/app/components/book-detail/book-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BooksService } from 'src/app/services/books.service';
+import { ReadlistService } from 'src/app/services/readlist.service';
 
 @Component({
   selector: 'app-book-detail',
@@ -8,6 +9,8 @@ import { BooksService } from 'src/app/services/books.service';
   styleUrls: ['./book-detail.component.css']
 })
 export class BookDetailComponent implements OnInit {
+  public allBooks: any;
+  public currentBook: any;
 
   public bookISBN: any;
   public bookTitle: any;
@@ -18,8 +21,9 @@ export class BookDetailComponent implements OnInit {
   public book: any;
   
 
-  constructor(private bookService: BooksService, private route: ActivatedRoute) { 
+  constructor(private bookService: BooksService, private route: ActivatedRoute, private readlistService: ReadlistService) { 
     let isbn = this.route.snapshot.paramMap.get('ISBN');
+
     let bookString = JSON.stringify(this.bookService.getBookByISBN(isbn));
     let bookJSON = JSON.parse(bookString);
 
@@ -32,7 +36,20 @@ export class BookDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    
   }
 
+  public addBook(book: any) {
+    let books = []; 
+
+    this.bookService.getAllBooks().subscribe((data) => {
+      let isbn = this.route.snapshot.paramMap.get('ISBN');
+      books = data;
+      for (let i = 0; i <books.length; i++){
+        if (books[i].isbn == isbn){
+          this.currentBook = books[i];
+          this.readlistService.addReadlistEntry(this.currentBook);
+        }
+      }
+    });
+  }
 }
